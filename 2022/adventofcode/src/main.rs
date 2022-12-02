82e008411aa33564ded17c5b0f2201cc01c4ee1d
Chris Gilmer <cgilmer@lucasfilm.com>
2022-12-02 10:23:29 -0800

    Write code for part 1 of problem 2. Add part 2 text.

--- 2022/adventofcode/src/main.rs
+++ 2022/adventofcode/src/main.rs
@@ -20,6 +20,7 @@ fn main() {
 
     match problem_num {
         1 => problem_01(filename),
+        2 => problem_02(filename),
         _ => std::process::exit(1),
     }
 }
@@ -30,9 +31,7 @@ where
 {
     let mut elves = Vec::new();
 
-    // File hosts must exist in current path before this produces output
     if let Ok(lines) = read_lines(filename) {
-        // Consumes the iterator, returns an (Optional) String
         let mut sum: i32 = 0;
         let mut elf: i32 = 1;
         for line in lines {
@@ -54,6 +53,77 @@ where
         let total_cals = elves[0].0 + elves[1].0 + elves[2].0;
         println!("Total Calories: {}", total_cals)
     }
+}
+
+// Problem Comments
+// A,X - Rock (1)
+// B,Y - Paper (2)
+// C,Z - Scissors (3)
+// Lose (0)
+// Draw (3)
+// Win (6)
+fn problem_02<P>(filename: P)
+where
+    P: AsRef<Path>,
+{
+    let mut points_l: i32 = 0;
+    let mut points_r: i32 = 0;
+    if let Ok(lines) = read_lines(filename) {
+        for line in lines {
+            if let Ok(ip) = line {
+                match ip.as_str() {
+                    "A X" => {
+                        // println!("draw");
+                        points_l = points_l + 1 + 3;
+                        points_r = points_r + 1 + 3;
+                    }
+                    "A Y" => {
+                        // println!("lose");
+                        points_l = points_l + 1 + 0;
+                        points_r = points_r + 2 + 6;
+                    }
+                    "A Z" => {
+                        // println!("win");
+                        points_l = points_l + 1 + 6;
+                        points_r = points_r + 3 + 0;
+                    }
+                    "B X" => {
+                        // println!("win");
+                        points_l = points_l + 2 + 6;
+                        points_r = points_r + 1 + 0;
+                    }
+                    "B Y" => {
+                        // println!("draw");
+                        points_l = points_l + 2 + 3;
+                        points_r = points_r + 2 + 3;
+                    }
+                    "B Z" => {
+                        // println!("lose");
+                        points_l = points_l + 2 + 0;
+                        points_r = points_r + 3 + 6;
+                    }
+                    "C X" => {
+                        // println!("lose");
+                        points_l = points_l + 3 + 0;
+                        points_r = points_r + 1 + 6;
+                    }
+                    "C Y" => {
+                        // println!("win");
+                        points_l = points_l + 3 + 6;
+                        points_r = points_r + 2 + 0;
+                    }
+                    "C Z" => {
+                        // println!("draw");
+                        points_l = points_l + 3 + 3;
+                        points_r = points_r + 3 + 3;
+                    }
+                    _ => continue,
+                }
+            }
+        }
+    }
+    println!("Points for opponent: {}", points_l);
+    println!("Points for you: {}", points_r);
 }
 
 // The output is wrapped in a Result to allow matching on errors
