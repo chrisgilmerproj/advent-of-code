4ccedc3103f3fcb9d9405a481b0bb162511c2b9b
Chris Gilmer <cgilmer@lucasfilm.com>
2022-12-07 14:12:13 -0800

    Complete prob 6 part 2

--- 2022/adventofcode/src/prob06.rs
+++ 2022/adventofcode/src/prob06.rs
@@ -7,19 +7,20 @@ pub fn problem<P>(filename: P)
 where
     P: AsRef<Path>,
 {
+    let search_size = 14;
     if let Ok(lines) = read_lines(filename) {
         for line in lines {
             if let Ok(ip) = line {
                 // println!("{}", ip);
                 let mut buffer: Vec<char> = Vec::new();
                 for (i, letter) in ip.chars().enumerate() {
-                    if buffer.len() == 4 {
+                    if buffer.len() == search_size {
                         buffer.remove(0);
                     }
                     buffer.push(letter);
                     let signal: HashSet<char> = HashSet::from_iter(buffer.iter().cloned());
                     // println!("{} {:?} {:?}", letter, buffer, signal);
-                    if signal.len() == 4 {
+                    if signal.len() == search_size {
                         println!("Signal found starting at {}", i + 1);
                         break;
                     }
