12e17a11b01eed7284b3a3bac15b89d5d57fb1ba
Chris Gilmer <cgilmer@lucasfilm.com>
2022-12-22 11:56:30 -0800

    Complete prob 10 part 2

--- 2022/adventofcode/src/prob10.rs
+++ 2022/adventofcode/src/prob10.rs
@@ -11,10 +11,11 @@ where
         let mut cycle_counter: i32 = 0;
         let important_cycles: Vec<i32> = vec![20, 60, 100, 140, 180, 220];
         let mut register_sum: i32 = 0;
+        let mut crt: Vec<String> = Vec::new();
+        let screen_size: usize = 40;
         for line in lines {
             if let Ok(ip) = line {
                 let instructions: Vec<&str> = ip.split(" ").collect();
-                // println!("{:?}", instructions);
                 let mut cycles: i32 = 0;
                 let mut val: i32 = 0;
                 match instructions[0] {
@@ -28,10 +29,15 @@ where
                     &_ => todo!(),
                 }
                 for i in 0..cycles {
+                    cycle_counter += 1;
+                    if (register - (crt.len() as i32 % screen_size as i32)).abs() <= 1 {
+                        crt.push("#".to_string());
+                    } else {
+                        crt.push(".".to_string());
+                    }
                     if i > 0 {
                         register += val;
                     }
-                    cycle_counter += 1;
                     if important_cycles.contains(&cycle_counter) {
                         println!(
                             "{} {} {}",
@@ -43,6 +49,20 @@ where
                     }
                 }
             }
+        }
+        // Print the screen
+        let mut screen_marker: usize = 0;
+        println!("\nScreen: ");
+        while screen_marker < crt.len() {
+            if crt.len() - screen_marker <= screen_size {
+                println!("b {}", crt[screen_marker..].join(""));
+            } else {
+                println!(
+                    "a {}",
+                    crt[screen_marker..screen_marker + screen_size - 1].join("")
+                );
+            }
+            screen_marker += screen_size;
         }
         println!("Register Sum: {}", register_sum);
     }
